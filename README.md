8edac736e426be5895359ec9524f0e30fd10664f
Nicolas Klin <nicolas.klin@tuhh.de>
2024-01-27 06:40:51 +0100

    Change image on window with keys 1,2,3

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Want to do a solo project in c. We'll see what it becomes.
 
 #SDL Tutorial
 Coding Made easy on youtube
+
+
+# To compile run
+gcc main.c -lSDL2main -lSDL2
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <stdbool.h>
 
 #define WINDOW_WIDTH 640
@@ -16,31 +16,85 @@ int main() {
         return 1;
     }
 
+    /*
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL) {
         printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
         return 1;
     }
+    */
 
+    SDL_Surface * window_surface = SDL_GetWindowSurface(window);
+    if (window_surface == NULL) {
+      printf("SDL_GetWindowSurface Error: %s\n", SDL_GetError());
+      return 1;
+    }
+
+    SDL_Surface * image_surface1 = SDL_LoadBMP("test.bmp");
+    if (image_surface1 == NULL) {
+      printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+      return 1;
+    }
+
+    SDL_Surface * image_surface2 = SDL_LoadBMP("test2.bmp");
+    if (image_surface2 == NULL) {
+      printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+      return 1;
+    }
+    SDL_Surface * image_surface3 = SDL_LoadBMP("test3.bmp");
+    if (image_surface3 == NULL) {
+      printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+      return 1;
+    }
+
+    SDL_Surface * current_image = image_surface1;
+
+    /*
     SDL_SetRenderDrawColor(renderer, 0, 0, 100, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
+    */
 
-    bool should_quit = false;
+    bool is_running = true;
+    SDL_Event event;
 
-    while (!should_quit) {
-      SDL_Event event;
+    while (is_running) {   //Gameloop
 
-      while (SDL_PollEvent(&event)) {
+      while (SDL_PollEvent(&event)) {   //Eventloop (something happens->event queue->SDL_Pollevent processes these)
         switch (event.type) {
+
           case SDL_QUIT:
-            should_quit = true;
+            is_running = false;
             break;
+
+          case SDL_KEYDOWN:
+            switch(event.key.keysym.sym) {
+
+              case SDLK_1:
+                current_image = image_surface1;
+                break;
+              case SDLK_2:
+                current_image = image_surface2;
+                break;
+              case SDLK_3:
+                current_image = image_surface3;
+                break;
+            }
+
           default:
             break;
         }
       }
+      SDL_BlitSurface(current_image, NULL, window_surface, NULL);
+      SDL_UpdateWindowSurface(window);
     }
+
+    SDL_FreeSurface(image_surface1);
+    SDL_FreeSurface(image_surface2);
+    SDL_FreeSurface(image_surface3);
+
+    current_image = image_surface1 = image_surface2 = image_surface3 = NULL;
+    SDL_Quit();
 
     return 0;
 }
